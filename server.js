cb3ede939bab5d5afd9f04f6d425d8b72e6e8930
shylag1073 <shylag1073@gmail.com>
2022-04-21 21:54:14 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 
 mongoose.set('debug', true);
