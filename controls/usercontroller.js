43cce94fb6101aeac1f8813afa1a272b9235775b
shylag1073 <shylag1073@gmail.com>
2022-04-21 20:03:53 -0500

    add user controls

---
+++ controls/usercontroller.js
@@ -0,0 +1,91 @@
+const { User, Thought } = require('../models');
+
+const userController ={
+
+
+// GET ALL USERS // 
+getAllUsers(req,res) {
+    User.find({})
+    .then(dbUserData => res.json(dbUserData))
+    .catch (err => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+},
+
+// GET UNO USER // 
+
+getSingleUser(req, res) {
+    User.findOne ({ _id: req.params.userId  })
+    .select('-_v')
+    .populate('thoughts')
+    .then ((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: ' Opps No user with this id! '});
+        }
+        res.json(dbUserData);
+    })
+    .catch ((err) => {
+        res.status(500).json(err);
+        console.log(err);
+    })
+
+},
+
+// MAKE A NEW USER // 
+
+createUser(req,res) {
+    User.create(req.body)
+    .then((dbUserData) => {
+        res.json(dbUserData);
+    })
+    .catch ((err) => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+},
+
+// UPDATE USER // 
+
+updateUser(req, res ) {
+    User.findOneAndUpdate (
+        { _id: req.params.userId },
+        {$set: req.body,},
+        {runValidators:true,
+        new: true,
+    })
+    .then ((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: ' Opps No user with this id! '});
+        }
+        res.json(dbUserData);
+    })
+    .catch ((err) => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
