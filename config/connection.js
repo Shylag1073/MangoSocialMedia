093458dbed94ff15e4f28e09dd4b0d1842108eb8
shylag1073 <shylag1073@gmail.com>
2022-04-27 21:39:33 -0500

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongob://localhost/socialmedia', {
-    useNewUrlParser: true,
-    UseUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia',
+// mongodb+srv://Shylar:Password@cluster0.8ivwd.mongodb.net/socialmedia?retryWrites=true&w=majority
+{
+useNewUrlParser:true,
+useUnifiedTopology:true
+}
+
+);
 
 module.exports = mongoose.connection;
